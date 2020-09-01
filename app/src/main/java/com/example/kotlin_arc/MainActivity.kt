398cdfad5989ce738263292f9a0ba4103e395bfb
MithuBitz <mithubortamuly@gmail.com>
2020-09-01 13:08:28 +0530

    Implecit and explicit intent

--- app/src/main/java/com/example/kotlin_arc/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_arc/MainActivity.kt
@@ -1,74 +1,19 @@
 package com.example.kotlin_arc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
-import android.widget.TextView
-
-import androidx.lifecycle.Observer
-
-import androidx.lifecycle.ViewModelProviders
-
-import com.example.kotlin_arc.viewModel.DiceViewModel
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var viewModel : DiceViewModel
-
-
-    private val imageViews by lazy {
-        arrayOf<ImageView>(
-            findViewById(R.id.die1),
-            findViewById(R.id.die2),
-            findViewById(R.id.die3),
-            findViewById(R.id.die4),
-            findViewById(R.id.die5)
-        )
-    }
-
-    private val headline by lazy {
-        findViewById<TextView>(R.id.headline)
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewModel = ViewModelProviders.of(this)
-            .get(DiceViewModel::class.java)
-
-        viewModel.headline.observe(this, Observer {
-            headline.text = it
-        })
-        viewModel.dice.observe(this, Observer {
-            updateDisplay(it)
-        })
-
-
-        lifecycle.addObserver(MyLifecycleObserver())
-
-        fab.setOnClickListener { viewModel.rollDice() }
-
-    }
-
-    private fun updateDisplay(dice: IntArray) {
-        for (i in 0 until imageViews.size) {
-            val drawableId = when (dice[i]) {
-                1 -> R.drawable.die_1
-                2 -> R.drawable.die_2
-                3 -> R.drawable.die_3
-                4 -> R.drawable.die_4
-                5 -> R.drawable.die_5
-                6 -> R.drawable.die_6
-                else -> R.drawable.die_6
-
-            }
-            imageViews[i].setImageResource(drawableId)
+        rollButton.setOnClickListener {
+            val intent = Intent(this, DiceActivity::class.java)
+            startActivity(intent)
         }
-
     }
 
 }
