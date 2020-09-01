48a9d280b9211e06e519cc0b42ab27cc0e249f6a
MithuBitz <mithubortamuly@gmail.com>
2020-09-01 13:55:12 +0530

    menu item about in action..

--- app/src/main/java/com/example/kotlin_arc/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_arc/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.kotlin_arc
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +16,19 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, DiceActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item?.itemId == R.id.action_about) {
+            AboutActivity.start(this)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
